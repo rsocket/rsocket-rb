6815f65e0d025fc7a35c57f7d0b96600b60bd421
linux_china <libing.chen@gmail.com>
2019-04-11 15:59:21 -0700

    add asserts

--- test/byte_buffer_test.rb
+++ test/byte_buffer_test.rb
@@ -6,32 +6,32 @@ class ByteBufferTest < Minitest::Test
 
   def test_init
     buffer = ByteBuffer.init(3)
-    puts buffer.size
+    assert_equal 3, buffer.size
   end
 
   def test_new
     bytes = Array.new(3, 0x11)
     buffer = ByteBuffer.new(bytes)
-    puts buffer.size
+    assert_equal 3, buffer.size
   end
 
-  def test_get
+  def test_put
     buffer = ByteBuffer.new(Array.new(3, 0x11))
     buffer.put 0x12
-    puts buffer.offset(0)
+    assert_equal 0x12, buffer.offset(0)
   end
 
   def test_put_bytes
     buffer = ByteBuffer.new(Array.new(3, 0x11))
-    buffer.put_bytes(Array.new(2, 0x22))
-    puts buffer.offset(1)
+    buffer.put_bytes([0x22, 0x23])
+    assert_equal 0x22, buffer.offset(0)
   end
 
   def test_get_int32
     buffer = ByteBuffer.new([0x00, 0x00, 0x00, 0x01])
-    assert_equal(1, buffer.get_int32)
+    assert_equal 1, buffer.get_int32
     buffer = ByteBuffer.new([0x00, 0x00, 0x02, 0x01])
-    assert_equal(2, buffer.get_int24)
+    assert_equal 2, buffer.get_int24
   end
 end
 
