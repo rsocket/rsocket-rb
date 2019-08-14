7af4bf58a2bfef7f7f08d2afede8f6041eb6abc7
linux_china <libing.chen@gmail.com>
2019-08-14 14:58:26 -0700

    payload test

---
+++ test/payload_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+require 'rsocket/payload'
+
+class PayloadTest < Minitest::Test
+
+  def test_creation
+     payload = payload_of([100, 97, 116, 97],[109, 101, 116, 97, 100, 97, 116, 97])
+     print payload.data_utf8
+     print payload.metadata_utf8
+  end
+
+  def test_parse_setup_payload
+    setup_payload = [0, 0, 43, 0, 0, 0, 0, 5, 0, 0, 1, 0, 0, 0, 0, 117, 48, 0, 1, 95, 144, 10, 116, 101, 120, 116, 47, 112, 108, 97, 105, 110, 10, 116, 101, 120, 116, 47, 112, 108, 97, 105, 110, 0, 0, 0]
+
+  end
+
+  def test_parse_request_response
+    payload = [0, 0, 23, 0, 0, 0, 1, 17, 0, 0, 0, 8, 109, 101, 116, 97, 100, 97, 116, 97, 100, 97, 116, 97, 45, 49]
+  end
+
+end
+
