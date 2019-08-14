e9707499a513b1facb37ae605ec97f8554588758
linux_china <libing.chen@gmail.com>
2019-08-14 15:31:35 -0700

    add test_length

--- test/payload_test.rb
+++ test/payload_test.rb
@@ -5,9 +5,17 @@ require 'rsocket/payload'
 class PayloadTest < Minitest::Test
 
   def test_creation
-     payload = payload_of([100, 97, 116, 97],[109, 101, 116, 97, 100, 97, 116, 97])
-     print payload.data_utf8
-     print payload.metadata_utf8
+    payload = payload_of([100, 97, 116, 97], [109, 101, 116, 97, 100, 97, 116, 97])
+    print payload.data_utf8
+    print payload.metadata_utf8
+    assert_equal "data", payload.data_utf8
+  end
+
+  def test_length
+    payload = payload_of([100, 97, 116, 97], [109, 101, 116, 97, 100, 97, 116, 97])
+    assert_equal 12, payload.bytes_length
+    payload = payload_of(nil, [109, 101, 116, 97, 100, 97, 116, 97])
+    assert_equal 8, payload.bytes_length
   end
 
   def test_parse_setup_payload
