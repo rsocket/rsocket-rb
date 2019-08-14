2a93c973091153a8d7ff4b206b6a42ab51df7912
linux_china <libing.chen@gmail.com>
2019-08-13 20:58:04 -0700

    add listener

---
+++ lib/rsocket.rb
@@ -0,0 +1,36 @@
+require 'rubygems'
+require 'eventmachine'
+
+$options = Hash[:port => 42252, :schema => "tcp"]
+
+module RSocketResponder
+
+  def set(option)
+    $options[:port] = option[:port]
+  end
+
+end
+
+module RSocketServer
+
+  include RSocketResponder
+
+  def post_init
+    puts "-- someone connected to the echo server!"
+  end
+
+  def receive_data data
+    send_data ">>> you sent: #{data}"
+    request_response data
+  end
+end
+
+extend RSocketResponder
+
+
+at_exit do
+  EventMachine::run {
+    EventMachine::start_server "0.0.0.0", $options[:port], RSocketServer
+    puts "RSocket Server on #{$options[:port]}"
+  }
+end
