f83f22f0a9732aed5b68da85758251c1c0f6f12a
linux_china <libing.chen@gmail.com>
2019-11-07 14:14:51 -0800

    adjust api

--- server.rb
+++ server.rb
@@ -1,14 +1,27 @@
-require './lib/rsocket'
-require './lib/rsocket/payload'
+require 'rsocket/server_bootstrap'
+require 'rsocket/payload'
+require 'rx'
 
 set :schema, 'tcp'
-set :port, 42253
+set :port, 42252
 
 
-# RSocket request/response
-# @param payload [Payload] rsocket payload
+# @param payload [RSocket::Payload]
+#@return [Rx::Observable]
 def request_response(payload)
-  print "RPC called"
-  payload_of("data", "metadata")
+  puts "received request_response: #{payload.data_utf8}"
+  Rx::Observable.just(payload_of("data", "metadata"))
+end
+
+# @param payload [RSocket::Payload]
+#@return [Rx::Observable]
+def request_stream(payload)
+  print "request/stream called"
+  Rx::Observable.from_array([payload_of("first", "metadata"), payload_of("second", "metadata")])
+end
+
+# @param payload [RSocket::Payload]
+def fire_and_forget(payload)
+  print "fire_and_forget"
 end
 
