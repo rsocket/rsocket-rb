5b7d67dfda1932a695374e8b6c59ee09c1dedf80
linux_china <libing.chen@gmail.com>
2019-11-07 14:13:20 -0800

    api adjusted

--- test/frame_test.rb
+++ test/frame_test.rb
@@ -7,15 +7,19 @@ class FrameTest < Minitest::Test
 
   def test_framing
     payload = payload_of("data-1".unpack("c*"), "metadata".unpack("c*"))
-    frame = RSocket::Frame.new(:REQUEST_RESPONSE, payload.metadata, payload.data, 1)
+    frame = RSocket::Frame.new(1, :REQUEST_RESPONSE)
+    frame.data = payload.data
+    frame.metadata = payload.metadata
     bytes = frame.serialize
     assert_equal bytes, [0x00, 0x00, 0x17, 0x00, 0x00, 0x00, 0x01, 0x11, 0x00, 0x00, 0x00, 0x08, 0x6d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0x2d, 0x31]
   end
 
   def test_parse
     payload = payload_of("data-1".unpack("c*"), "metadata".unpack("c*"))
-    frame = RSocket::Frame.new(:REQUEST_RESPONSE, payload.metadata, payload.data, 1)
-    frame2 = RSocket::Frame.parse frame.serialize
+    frame = RSocket::Frame.new(1, :REQUEST_RESPONSE)
+    frame.metadata = payload.metadata
+    frame.data = payload.data
+    frame2 = RSocket::Frame.parse(frame.serialize)
     assert_equal frame2.frame_type, frame.frame_type
   end
 
