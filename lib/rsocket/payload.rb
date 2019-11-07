671d9ad701aaf52ee6a80c63259aecb47fcf8747
linux_china <libing.chen@gmail.com>
2019-11-07 14:10:50 -0800

    add Connection setup payload

--- lib/rsocket/payload.rb
+++ lib/rsocket/payload.rb
@@ -15,14 +15,15 @@ module RSocket
     def metadata
       @metadata
     end
+
     # @return [String, nil] the contents of our object or nil
     def metadata_utf8
-      @metadata.nil? ? nil : @metadata.pack('c*')
+      @metadata.nil? ? nil : @metadata.pack('C*')
     end
 
     # @return [String, nil] the contents of our object or nil
     def data_utf8
-      @data.nil? ? nil : @data.pack('c*')
+      @data.nil? ? nil : @data.pack('C*')
     end
 
     # @return [Integer] bytes length
@@ -31,10 +32,24 @@ module RSocket
     end
   end
 
+  class ConnectionSetupPayload < Payload
+    attr_accessor :metadata_mime_type, :data_mime_type
+
+    def initialize(metadata_mime_type = "text/plain", data_mime_type = "text/plain", data, metadata)
+      @metadata_mime_type = metadata_mime_type
+      @data_mime_type = data_mime_type
+      @data = data
+      @metadata = metadata
+    end
+
+  end
+
 end
 
-# @param data [Array<Byte>] data array
-# @param metadata [Array<Byte>] metadata array
+# @param data [Array<Byte>, String]
+# @param metadata [Array<Byte>, String]
 def payload_of(data, metadata)
-  RSocket::Payload.new(data, metadata)
+  data_bytes = !data.nil? && data.is_a?(String) ? data.unpack('C*') : data
+  metadata_bytes = !metadata.nil? && metadata.is_a?(String) ? metadata.unpack('C*') : metadata
+  RSocket::Payload.new(data_bytes, metadata_bytes)
 end
