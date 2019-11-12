ca6f1a37341f14543142c79b250d77bca1114b73
linux_china <libing.chen@gmail.com>
2019-11-12 17:31:31 +0800

    code clean

--- server.rb
+++ server.rb
@@ -1,16 +1,13 @@
+require 'rsocket'
 require 'rsocket/server_bootstrap'
-require 'rsocket/payload'
-require 'rx'
 
 set :schema, 'tcp'
 set :port, 42252
 
-
 # @param payload [RSocket::Payload]
 #@return [Rx::Observable]
 def request_response(payload)
-  puts "received request_response: #{payload.data_utf8}"
-  Rx::Observable.just(payload_of("data", "metadata"))
+  Rx::Observable.just(payload_of("Hello #{payload.data_utf8}", nil))
 end
 
 # @param payload [RSocket::Payload]
