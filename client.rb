ad2abafe13427822ac94369d363f146d7da42f1b
linux_china <libing.chen@gmail.com>
2020-01-29 15:44:29 -0800

    add metadata

--- client.rb
+++ client.rb
@@ -1,18 +1,24 @@
 require 'rubygems'
-require 'eventmachine'
+require 'rsocket'
 require 'rsocket/requester'
-require 'rsocket/payload'
 require 'rx'
 
 
 EventMachine.run {
-  rsocket = RSocket.connect("tcp://127.0.0.1:42252") do
+  setup_composite = RSocket::CompositeMetadata.new
+  setup_composite.add_wellknown_metadata(:RSOCKET_BEARER_TOKEN, "jwt_token".bytes.to_a)
+  rsocket = RSocket.connect("tcp://127.0.0.1:42253", setup_payload: payload_of(nil, setup_composite.to_bytes)) do
     def request_response(payload)
       puts "request_response received: #{payload.data_utf8}"
       Rx::Observable.just(payload_of("data", "metadata"))
     end
   end
-  rsocket.request_response(payload_of("request", "response"))
+
+  metadata = RSocket::CompositeMetadata.new
+
+  metadata.add_wellknown_metadata(:RSOCKET_MESSAGE_MIMETYPE, RSocket::data_encoding_metadata_byte(:TEXT_PLAIN))
+  metadata.add_wellknown_metadata(:RSOCKET_ACCEPT_MIMETYPES, RSocket::accept_encodings_metadata_bytes([:TEXT_PLAIN]))
+  rsocket.request_response(payload_of("hello", metadata.to_bytes))
       .subscribe(Rx::Observer.configure do |observer|
         observer.on_next { |payload| puts payload.data_utf8 }
         observer.on_completed { puts "completed" }
